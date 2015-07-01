633060640da4f28af8ed6bd877938052a57fcbe7
Oleksiy Stetsenko <oleksiys@interlink-ua.com>
2015-07-01 14:24:42 +0300

    form add new user

---
+++ src/main/webapp/scripts/app/entities/user/user.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('mypalApp')
+    .controller('UserController', function ($scope, User) {
+        $scope.users = [];
+        $scope.loadAll = function() {
+            User.query(function(result) {
+               $scope.users = result;
+            });
+        };
+        $scope.loadAll();
+
+        $scope.save = function () {
+            if ($scope.user.id != null) {
+                User.update($scope.user,
+                    function () {
+                        $scope.refresh();
+                    });
+            } else {
+                User.save($scope.user,
+                    function () {
+                        $scope.refresh();
+                    });
+            }
+        };
+
+        $scope.clear = function () {
+            $scope.user = {title: null, id: null};
+            $scope.editForm.$setPristine();
+            $scope.editForm.$setUntouched();
+        };
+    });
---
+++ src/main/webapp/scripts/app/entities/user/user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('mypalApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('user', {
+                parent: 'entity',
+                url: '/user',
+                data: {
+                    roles: ['ROLE_ANONYMOUS','ROLE_ADMIN','ROLE_USER'],
+                    pageTitle: 'Users'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/user/users.html',
+                        controller: 'UserController'
+                    }
+                },
+                resolve: {}
+            })
+    });
