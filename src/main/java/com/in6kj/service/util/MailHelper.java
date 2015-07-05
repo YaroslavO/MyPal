a5f328fb3184f7552c5fd83191a11a65307371fa
YaroslavO <JaroslavO@softwareplanet.uk.com>
2015-07-05 20:56:29 +0300

    write method for delete email

--- src/main/java/com/in6kj/service/util/MailHelper.java
+++ src/main/java/com/in6kj/service/util/MailHelper.java
@@ -53,5 +53,76 @@ public class MailHelper {
         catch (IOException e) {e.printStackTrace();}
     }
 
+    public static void deleteEmail() throws Exception {
+        String user= "sonoojaiswal@javatpoint.com";//change accordingly
+        String password="xxxxx";//change accordingly
 
+        //1) get the session object
+        Properties properties = System.getProperties();
+        Session session = Session.getDefaultInstance(properties);
+
+        //2) create the store object and connect to the current host
+        Store store = session.getStore("pop3");
+        store.connect("mail.javatpoint.com",user,password);
+
+        //3) create the folder object and open it
+        Folder folder = store.getFolder("inbox");
+
+        if (!folder.exists()) {
+            System.out.println("inbox not found");
+            System.exit(0);
+        }
+
+        folder.open(Folder.READ_WRITE);
+
+        //4) Get the message to delete
+        Message[] msg = folder.getMessages();
+
+        //System.out.println((messages.length+1)+" message found");
+        for (int i = 0; i < msg.length; i++) {
+            System.out.println("--------- " + (i + 1) + "------------");
+            String from = InternetAddress.toString(msg[i].getFrom());
+
+            if (from != null) {
+                System.out.println("From: " + from);
+            }
+
+            String replyTo = InternetAddress.toString(
+                msg[i].getReplyTo());
+            if (replyTo != null) {
+                System.out.println("Reply-to: " + replyTo);
+            }
+
+            String to = InternetAddress.toString(
+                msg[i].getRecipients(Message.RecipientType.TO));
+
+            if (to != null) {
+                System.out.println("To: " + to);
+            }
+
+            String subject = msg[i].getSubject();
+            if (subject != null) {
+                System.out.println("Subject: " + subject);
+            }
+            Date sent = msg[i].getSentDate();
+            if (sent != null) {
+                System.out.println("Sent: " + sent);
+            }
+            System.out.println("Message : ");
+            System.out.println(msg[i].getContent());
+
+        }//end of for loop
+
+        // get the message number to delete (optional)
+        System.out.println("Enter message number to delete :");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String no = br.readLine();
+        //5) delete the message using setFlag method
+        msg[Integer.parseInt(no) - 1].setFlag(FLAGS.Flag.DELETED, true);
+
+        System.out.println("Message Deleted .....");
+
+        folder.close(true);
+        store.close();
+    }
 }
